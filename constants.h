e9865194f3dbd51b9c46a904d8a2119d8b7a4a93
yanchengzeng <yz203@duke.edu>
2015-03-01 14:54:48 -0500

    added node and constants header files

---
+++ constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_ROUTES 128
+#define MAX_TTL 120
+
+
+#endif
--- node.h
+++ node.h
@@ -1,14 +1,29 @@
-struct phySocket {
-	sockaddr_in ip;
-	int port;
+#ifndef NODE_H
+#define NODE_H
+
+#include <vector>
+#include <string>
+#include <stdlib>
+#include <sys/socket>
+#include <arpa/inet.h>
+#include <unistd>
+#include <netdb>
+#include "constants.h"
+
+
+using namespace std;
+
+
+typedef struct {
+	string des;
+	int cost;
+	string nextHop;
+} RouteEntry;
+
+class Node{
+
+	private:
+		RouteEntry localTable[MAX_ROUTES];
+		sockaddr_in nodeInfo;
+
 };
-
-struct routerPort {
-	phySocket ps;
-	sockaddr_in vipLoc;
-	sockaddr_in vipRem;
-};
-
-phySocket parsePhySocket(string config);
-
-sockaddr_in stringToIP(string sip);
